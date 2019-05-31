377657506d89bb68c0ac75e15591f4f62c6ab8db
matejkolarczyk <m.kolarczyk.st@spseiostrava.cz>
2019-05-31 08:59:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+//import React from 'react';
+import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import ProductList from "./components/ProductList";
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <React.Fragment>
+         <Navbar></Navbar>
+         <Switch>
+            <Route exact path='/' component={ProductList}></Route>
+           <Route path="/details" component={Details}></Route>
+           <Route path="/cart" component={Cart}></Route>
+           <Route component={Default}></Route>
+        </Switch>
+         </React.Fragment> 
+      </div>
+    );
+  }
 }
 
 export default App;
+/*function App() {
+  return (
+  <React.Fragment>
+   <h3>Hello from App.js</h3>
+  </React.Fragment>  
+  );
+}
+
+export default App;*/
---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Product from './Product';
+
+class productList extends Component {
+    render() {
+        return (
+            <div>
+                <Product></Product>
+            </div>
+        );
+    }
+}
+
+export default productList;
